1099c0c5f2670ae833403b2861add3e49cc421f2
Theofanis Tsiantas <th.tsiantas@icloud.com>
2022-01-10 10:59:32 +0200

    Sliding door programed in AddUserPage.

---
+++ src/main/resources/static/js/coreAddUserPage.js
@@ -0,0 +1,14 @@
+// Function to move image slowly every some
+// milliseconds.
+function moveImage(){
+    for (let i = 0; i < 20; i++) {
+        setTimeout(move, i*100, i);
+    }
+}
+
+// Fuction that perfomrs the immage move in a certain direction
+function move(i) {
+    // The monster image is the 4rth image element of the LoginPage.
+    var imageToMove = document.getElementsByTagName("img")[3];
+    imageToMove.style.left = (-2.7+0.1*i)+"cm";
+}
